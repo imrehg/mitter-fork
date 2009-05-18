e2479a6dcc46da9ba8c110345aca093a06c1d1a5
Gergely Imreh <imrehg@gmail.com>
2009-05-18 18:30:25 +0800

    bump version number

--- mitterlib/constants.py
+++ mitterlib/constants.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-version = '0.4.5'
+version = '0.4.5-fork0.1'
 
 gpl_3 = """
                     GNU GENERAL PUBLIC LICENSE
